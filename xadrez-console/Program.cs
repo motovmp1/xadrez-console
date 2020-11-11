058fdd5554f2b6b5ed978873986eb44d2534a78e
motovmp1 <vinicius.mpinho@gmail.com>
2020-11-11 11:48:21 +0000

    Projeto criado

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console
+{
+    class Program
+    {
+#pragma warning disable IDE0060 // Remove unused parameter
+        static void Main(string[] args)
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+            Console.WriteLine("hello");
+
+        }
+    }
+}
